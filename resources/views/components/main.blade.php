72808d394d1d9fe86a3cfdc22e9b547becd9f03c
bismal  getter <ijazhooria321@gmail.com>
2026-03-13 16:14:52 +0500

    blds mn  csstyl
    blds mn  csstyl

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -1,14 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
-{{ $header ?? '' }}
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap -->
+    <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+
+    <!-- Your CSS -->
+    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
+
+    @stack('styles')
+</head>
 
 <body>
 
     {{ $navabr ?? '' }}
     {{ $sidebar ?? '' }}
-    @yield('content')
-    {{ $footer ?? '' }}
-    @yield('js')
-</body>
 
+    @yield('content')
+
+    {{ $footer ?? '' }}
+
+    @yield('js')
+
+    @stack('scripts')
+
+</body>
 </html>
