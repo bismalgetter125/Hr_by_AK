75611269260a84d5977bd5efb4660c4d725337ee
bismal  getter <ijazhooria321@gmail.com>
2026-03-13 16:18:35 +0500

    bck mnbld
    bck mnbld

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -1,30 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap -->
-    <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-
-    <!-- Your CSS -->
-    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
-
-    @stack('styles')
-</head>
+{{ $header ?? '' }}
 
 <body>
 
     {{ $navabr ?? '' }}
     {{ $sidebar ?? '' }}
-
     @yield('content')
-
     {{ $footer ?? '' }}
-
     @yield('js')
-
-    @stack('scripts')
-
 </body>
+
 </html>
